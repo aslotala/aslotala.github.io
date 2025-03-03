607e320a9a1640a9679c5ad062a3666fd624e56d
Arkadiusz Ślotała <as459548@students.mimuw.edu.pl>
2025-03-04 00:59:51 +0100

    added photos from site and upgraded the markdown generator

--- README.md
+++ README.md
@@ -2,34 +2,34 @@
 
 ## 7. Nine Men’s Morris
 photo source:Wikimedia Commons
-![7. Nine Men’s Morris](https://www.oldest.org/wp-content/uploads/2017/11/Nine-Mens-Morris.jpg)
+![7. Nine Men’s Morris](photos/7._nine_men’s_morris.jpg)
 
 
 ## 6. Go
 photo source:Wikimedia Commons
-![6. Go](https://www.oldest.org/wp-content/uploads/2017/11/Go.jpg)
+![6. Go](photos/6._go.jpg)
 
 
 ## 5. Royal Game of Ur
 photo source:Wikimedia Commons
-![5. Royal Game of Ur](https://www.oldest.org/wp-content/uploads/2017/11/Royal-Game-of-Ur.jpg)
+![5. Royal Game of Ur](photos/5._royal_game_of_ur.jpg)
 
 
 ## 4. Mehen
 photo source:Wikimedia Commons
-![4. Mehen](https://www.oldest.org/wp-content/uploads/2017/11/Mehen.jpg)
+![4. Mehen](photos/4._mehen.jpg)
 
 
 ## 3. Backgammon
 photo source:Wikimedia Commons
-![3. Backgammon](https://www.oldest.org/wp-content/uploads/2017/11/Backgammon.jpg)
+![3. Backgammon](photos/3._backgammon.jpg)
 
 
 ## 2. Checkers
 photo source:publicdomainpictures.net
-![2. Checkers](https://www.oldest.org/wp-content/uploads/2017/11/Checkers.jpg)
+![2. Checkers](photos/2._checkers.jpg)
 
 
 ## 1. Senet
 photo source:Wikimedia Commons
-![1. Senet](https://www.oldest.org/wp-content/uploads/2017/11/Senet.jpg)
+![1. Senet](photos/1._senet.jpg)
--- scraper.py
+++ scraper.py
@@ -3,18 +3,25 @@
 # My website - https://aslotala.github.io/
 # My site -    https://www.oldest.org/entertainment/board-games/
 
+import os
 import requests
 from bs4 import BeautifulSoup
 from markdownify import markdownify as md
 
-r = requests.get('https://www.oldest.org/entertainment/board-games/')
-start = '<h3>8. Chess</h3>'
-end = '<p>Although historians aren&#8217;t quite sure how exactly the game was played, Timothy Kendall and R.C. Bell have made their own reconstructions of the game. Kendall and Bell&#8217;s rules are based on pieces of texts mentioning Senet and these rules have been adopted by modern senet players.</p>'
+url = 'https://www.oldest.org/entertainment/board-games/'
 
-html = r.text
-cut_html = r.text.split(start)[1].split(end)[0]
+r = requests.get(url)
+
+start_marker = '<h3>8. Chess</h3>'
+end_marker = '<p>Although historians aren&#8217;t quite sure how exactly the game was played, Timothy Kendall and R.C. Bell have made their own reconstructions of the game. Kendall and Bell&#8217;s rules are based on pieces of texts mentioning Senet and these rules have been adopted by modern senet players.</p>'
+
+cut_html = r.text.split(start_marker)[1].split(end_marker)[0]
 
 soup = BeautifulSoup(cut_html, 'html.parser')
+
+img_folder = "photos"
+os.makedirs(img_folder, exist_ok=True)
+
 
 # Extract game titles, descriptions, and images
 games = []
@@ -26,9 +33,20 @@ for h3 in soup.find_all('h3'):
     game_info = f"## {game_name}\n"
     if description:
         game_info += f"{description.get_text(strip=True)}\n"
-    if image:
+
+    # Process images
+    if image and 'src' in image.attrs:
         img_url = image['src']
-        game_info += f"![{game_name}]({img_url})\n"
+        img_name = f"{game_name.lower().replace(' ', '_')}.jpg"
+        img_path = os.path.join(img_folder, img_name)
+
+        # Download the image
+        img_data = requests.get(img_url).content
+        with open(img_path, "wb") as img_file:
+            img_file.write(img_data)
+
+        # Add local image reference to Markdown
+        game_info += f"![{game_name}](photos/{img_name})\n"
 
     games.append(game_info)
 
@@ -39,4 +57,4 @@ markdown_content = "# Oldest Board Games\n\n" + "\n\n".join(games)
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(markdown_content)
 
-print("Scraped content saved to README.md")
+print("Scraped content saved to README.md, images downloaded to 'photos/'")
